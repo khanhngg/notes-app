baeb710dc9dd8742be65dd79a812276fca8fb326
Khanh Nguyen <knguyenn11@gmail.com>
2023-06-01 19:19:59 -0700

    Add CRUD functions for folders and notes to call REST API

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import { fetchFolders, fetchNotesFromFolder } from "api/api";
 
 function App() {
+  const [folders, setFolders] = useState([])
+  const [notes, setNotes] = useState([])
+  const [currentFolder, setCurrentFolder] = useState()
+  const [currentNote, setCurrentNote] = useState()
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const foldersData = await fetchFolders();
+      const notesData = await fetchNotesFromFolder(foldersData[0].id);
+
+      setFolders(foldersData);
+      setCurrentFolder(foldersData[0]);
+      setNotes(notesData);
+      setCurrentNote(notesData[0]);
+    } catch (error) {
+      // TODO: Handle error
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <header>header stuff here</header>
+      <div className="container">
+        <div className="folders-container">
+          {currentFolder && (<p>current folder is {currentFolder.id}</p>)}
+          {folders.map(({id, name}) => (
+            <p key={id}>{id}: {name}</p>
+          ))}
+        </div>
+        <div className="notes-container">
+          {currentNote && (<p>current note is {currentNote.id}</p>)}
+          {notes.map(({id, content, timeCreated, timeModified, folderId}) => (
+              <p key={id}>{id}: {content} {timeCreated} {timeModified} {folderId}</p>
+          ))}
+        </div>
+        <div className="notes-editor">
+          {currentNote && (<p>current note content is {currentNote.content}</p>)}
+        </div>
+      </div>
+    </>
   );
 }
 
---
+++ src/api/api.js
@@ -0,0 +1,136 @@
+const API_BASE_URL = "https://my-json-server.typicode.com/khanhngg/notes-app";
+
+// TODO: test out how new ID are handled? or need to pass it here?
+/**
+ * Creates a new folder.
+ *
+ * @param {Object} folderData - The data for the new folder.
+ * @returns {Promise<Object>} A Promise that resolves to the newly created folder.
+ */
+export const createFolder = async (folderData) => {
+  try {
+    const response = await fetch(`${API_BASE_URL}/folders`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(folderData),
+    });
+    return await response.json();
+  } catch (error) {
+    console.error('Error creating folder:', error);
+    throw error;
+  }
+};
+
+/**
+ * Creates a new note.
+ *
+ * @param {Object} noteData - The data for the new note.
+ * @returns {Promise<Object>} A Promise that resolves to the newly created note.
+ */
+export const createNote = async (noteData) => {
+  try {
+    const response = await fetch(`${API_BASE_URL}/notes`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(noteData),
+    });
+    return await response.json();
+  } catch (error) {
+    console.error('Error creating note:', error);
+    throw error;
+  }
+};
+
+/**
+ * Fetches folders.
+ *
+ * @returns {Promise<Array>} A Promise that resolves to an array of folder objects.
+ */
+export const fetchFolders = async () => {
+  try {
+    const response = await fetch(`${API_BASE_URL}/folders`);
+    return await response.json();
+  } catch (error) {
+    console.error('Error fetching folders:', error);
+    throw error;
+  }
+};
+
+/**
+ * Fetches notes from a folder by folder's ID.
+ *
+ * @param {number} folderId - The ID of the folder to fetch.
+ * @returns {Promise<Array>} A Promise that resolves to an array of note objects.
+ */
+export const fetchNotesFromFolder = async (folderId) => {
+  try {
+    const response = await fetch(`${API_BASE_URL}/folders/${folderId}/notes`);
+    return await response.json();
+  } catch (error) {
+    console.error(`Error fetching notes from folder with ID ${folderId}:`, error);
+    throw error;
+  }
+};
+
+/**
+ * Fetches a note by ID.
+ *
+ * @param {number} noteId - The ID of the note to fetch.
+ * @returns {Promise<Object>} A Promise that resolves to a note object.
+ */
+export const fetchNote = async (noteId) => {
+  try {
+    const response = await fetch(`${API_BASE_URL}/notes/${noteId}`);
+    return await response.json();
+  } catch (error) {
+    console.error(`Error fetching note with ID ${noteId}:`, error);
+    throw error;
+  }
+};
+
+/**
+ * Updates an existing note.
+ *
+ * @param {number} noteId - The ID of the note to update.
+ * @param {Object} noteData - The updated data for the note.
+ * @returns {Promise<Object>} A Promise that resolves to the updated note object.
+ */
+export const updateNote = async (noteId, noteData) => {
+  try {
+    const response = await fetch(`${API_BASE_URL}/notes/${noteId}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(noteData),
+    });
+    return await response.json();
+  } catch (error) {
+    console.error('Error updating note:', error);
+    throw error;
+  }
+};
+
+/**
+ * Deletes an existing note.
+ *
+ * @param {number} noteId - The ID of the note to delete.
+ * @returns {Promise<void>} A Promise that resolves when the note is successfully deleted.
+ */
+export const deleteClassroom = async (noteId) => {
+  try {
+    const response = await fetch(`${API_BASE_URL}/notes/${noteId}`, {
+      method: 'DELETE',
+    });
+    if (!response.ok) {
+      throw new Error('Failed to delete note');
+    }
+  } catch (error) {
+    console.error('Error deleting note:', error);
+    throw error;
+  }
+};
