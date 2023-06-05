09dfb26ddd664fa760802a879bc440861a3c0e14
Khanh Nguyen <knguyenn11@gmail.com>
2023-06-05 15:23:11 -0700

    Fix time parser display and add mock data to db.json

--- src/utils/timeParser.js
+++ src/utils/timeParser.js
@@ -1,10 +1,12 @@
 export const parseDateTime = (timestamp) => {
   const date = new Date(timestamp * 1000);
 
-  const dateString = date.toDateString().split(" ").slice(1).join(" ");
+  const dateArr = date.toDateString().split(" ").slice(1);
+  const dayMonthString = dateArr.slice(0, 2).join(" ");
+  const yearString = dateArr[2];
   const time = parseTime(timestamp);
 
-  return `${dateString} at ${time}`;
+  return `${dayMonthString}, ${yearString} at ${time}`;
 };
 
 export const parseTime = (timestamp) => {
