ef5ba080b477eda9f9d75e0cd76191f106f7f953
Khanh Nguyen <knguyenn11@gmail.com>
2023-06-05 15:04:09 -0700

    Add search bar and minor style fixes

--- src/utils/timeParser.js
+++ src/utils/timeParser.js
@@ -2,12 +2,9 @@ export const parseDateTime = (timestamp) => {
   const date = new Date(timestamp * 1000);
 
   const dateString = date.toDateString().split(" ").slice(1).join(" ");
+  const time = parseTime(timestamp);
 
-  const timeString = date.toLocaleTimeString('en-US').split(" ");
-  const time = timeString[0].split(':').slice(0, 2).join(':');
-  const period = timeString[1];
-
-  return `${dateString} at ${time} ${period}`;
+  return `${dateString} at ${time}`;
 };
 
 export const parseTime = (timestamp) => {
